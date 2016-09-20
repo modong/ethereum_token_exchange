6acbdbd813651cb79a3614e3a70dc3d34070f9c0
Mo Dong <montedong@gmail.com>
2016-09-20 00:57:44 -0500

    update

--- simple_exchange.py
+++ simple_exchange.py
@@ -2,7 +2,6 @@ from ethereum import tester, utils
 from rlp.utils import decode_hex, encode_hex, ascii_chr, str_to_bytes
 from ethereum import slogging
 import serpent
-
 slogging.configure(':INFO')
 s = tester.state()
 tokenCode = """
@@ -23,6 +22,7 @@ def send_token(to, value):
 
 exchangeCode = """
 extern contract: [send_token:[int256,int256]:_]
+extern contract: [transfer:[bytes]:_]
 extern contract: [get_balance:[int256]:int256]
 data owner
 data secret
@@ -30,18 +30,22 @@ data timeout
 data source
 data is_initialized
 data nounce
+data peer
+data token
 
 event Notice(s:str, x:uint256)
 def init():
     self.owner = msg.sender
     self.is_initialized = 0
 
-def initialize(secret, timeout, source):
+def initialize(secret, timeout, source, peer:address, token:address):
     if msg.sender == self.owner and self.is_initialized == 0:
         self.secret = secret
         self.timeout = timeout
         self.source = source
         self.is_initialized = 1
+        self.peer = peer
+        self.token = token
 
 def get_nounce():
     log(type=Notice, text("!!!"), temp)
@@ -51,13 +55,12 @@ def get_secret():
     if self.is_initialized:
         return self.secret
 
-def transfer(token:address, nounce:str):
-    temp = sha3(nounce, chars=len(nounce))
-    if temp == self.secret:
-        save(self.nounce, nounce,chars=len(nounce))
-        self.nounce = nounce
-        log(type=Notice, self.nounce, self.nounce)
-        token.send_token(self.owner, token.get_balance(self), sender=self)
+def transfer(nounce:str):
+    temp = sha3(nounce, chars=len(nounce)) 
+    if temp == self.secret and block.number < self.timeout:
+        self.token.send_token(self.owner, self.token.get_balance(self), sender=self)
+        if self.peer != 0:
+            self.peer.transfer(nounce)
 
 def refund(token:address):
     if block.number > self.timeout:
@@ -70,17 +73,13 @@ goldToken = s.abi_contract(tokenCode, sender=bob)
 exchangeAlicePart = s.abi_contract(exchangeCode, sender=alice)
 exchangeBobPart = s.abi_contract(exchangeCode, sender=bob)
 
-s.mine(100)
-pre_image = "10"
+pre_image = "10" 
 block_timeout = 50
-exchangeAlicePart.initialize(utils.sha3(pre_image), block_timeout, utils.privtoaddr(bob), sender=alice)
-exchangeBobPart.initialize(exchangeAlicePart.get_secret(), block_timeout, utils.privtoaddr(alice), sender=bob)
-
+exchangeAlicePart.initialize(utils.sha3(pre_image), block_timeout, utils.privtoaddr(bob), exchangeBobPart.address, goldToken.address, sender=alice)
+exchangeBobPart.initialize(exchangeAlicePart.get_secret(), block_timeout, utils.privtoaddr(alice), 0, silverToken.address, sender=bob)
 silverToken.send_token(exchangeBobPart.address, 500, sender=alice)
 goldToken.send_token(exchangeAlicePart.address, 500, sender=bob)
-exchangeAlicePart.transfer(goldToken.address, pre_image, sender=alice)
-print exchangeAlicePart.get_nounce()
-exchangeBobPart.transfer(silverToken.address, pre_image, sender=bob)
+exchangeAlicePart.transfer(pre_image, sender=alice)
 
 print silverToken.get_balance(utils.privtoaddr(alice))
 print silverToken.get_balance(utils.privtoaddr(bob))
